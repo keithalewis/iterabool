a1083800be00b722a5e108fe520160f59a43f8a8
Keith A. Lewis <keithalewis@live.com>
2020-11-22 23:38:48 -0500

    forward_sequence still need oprator++ constraint iota, array, apply, take

--- iterabool.h
+++ iterabool.h
@@ -1,23 +1,258 @@
 // Iterators with operator bool() const
 #pragma once
 #include <algorithm>
+#include <compare>
 #include <concepts>
 #include <iterator>
 #include <type_traits>
 
 namespace iterabool {
 
-	template<typename S>
-	concept sequence = std::input_iterator<S> 
-		&& requires (S s) { { !s }; };
-		/*
-		requires (S s) {
-		typename std::iterator_traits<S>::value_type;
+	template<class S>
+	concept forward_sequence = requires (const S s) {
+		typename S::value_type;
 		{ s.operator bool() } -> std::same_as<bool>;
-		{ s.operator*() } -> std::convertible_to<typename std::iterator_traits<S>::value_type>;
-		{ s.operator++() } -> std::convertible_to<S&>;
+		{ s.operator*() } -> std::convertible_to<typename S::value_type>;
+		//{ s.operator++() }
+	}
+	; //&& std::input_iterator<S>;
+
+	// t, t + 1, ...
+	template<typename T>
+	class iota {
+		T t;
+	public:
+		using iterator_concept = std::forward_iterator_tag;
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = T;
+		
+		iota(T t = 0)
+			: t(t)
+		{ }
+		iota(const iota&) = default;
+		iota& operator=(const iota&) = default;
+		~iota()
+		{ }
+
+		//auto operator<=>(const iota&) = default;
+		bool operator==(const iota& i) const
+		{
+			return t == i.t;
+		}
+
+		operator bool() const 
+		{
+			return true;
+		}
+		T operator*() const 
+		{
+			return t;
+		}
+		iota& operator++()
+		{
+			++t;
+
+			return *this;
+		}
 	};
-		*/
+
+	template<class T>
+	class array {
+		size_t n;
+		const T* a;
+	public:
+		using iterator_concept = std::forward_iterator_tag;
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = T;
+
+		array()
+			: n(0), a(nullptr)
+		{ }
+		template<size_t N>
+		array(const T(&a)[N])
+			: n(N), a(a)
+		{ }
+		array(const array&) = default;
+		array& operator=(const array&) = default;
+		~array()
+		{ }
+
+		bool operator==(const array& s) const
+		{
+			return n == s.n and a == s.a;
+		}
+		//auto operator<=>(const array&) = default;
+
+		// remaining size
+		size_t size() const
+		{
+			return n;
+		}
+
+		operator bool() const 
+		{
+			return n != 0;
+		}
+		/*typename value_type*/ int operator*() const
+		{
+			return *a;
+		}
+		array& operator++()
+		{
+			if (*this) {
+				--n;
+				++a;
+			}
+
+			return *this;
+		}
+	};
+
+	// take at most n items from a sequence
+	template<forward_sequence S>
+	class take {
+		size_t n;
+		S s;
+	public:
+		using iterator_concept = typename S::iterator_concept;
+		using iterator_category = typename S::iterator_category;
+		using value_type = typename S::value_type;
+
+		take()
+			: n(0)
+		{ }
+		take(size_t n, const S& s)
+			: n(n), s(s)
+		{ }
+		take(const take&) = default;
+		take& operator=(const take&) = default;
+		~take()
+		{ }
+
+		bool operator==(const take& t) const
+		{
+			return n == t.n and s == t.s;
+		}
+		//auto operator<=>(const take&) = default;
+
+		// remaining size
+		size_t size() const
+		{
+			return n;
+		}
+
+		operator bool() const
+		{
+			return s and n != 0;
+		}
+		value_type operator*() const
+		{
+			return *s;
+		}
+		take& operator++()
+		{
+			if (*this) {
+				--n;
+				++s;
+			}
+
+			return *this;
+		}
+	};
+
+	// apply function to sequence
+	template<class F, class S,
+		class T = S::value_type,
+		class U = std::invoke_result_t<F, T>>
+	class apply {
+		F f;
+		S s;
+	public:
+		using iterator_concept = typename S::iterator_concept;
+		using iterator_category = typename S::iterator_category;
+		using value_type = typename U;
+
+		apply()
+		{ }
+		apply(const F& f, const S& s)
+			: f(f), s(s)
+		{ }
+		apply(const apply&) = default;
+		apply& operator=(const apply&) = default;
+		~apply()
+		{ }
+
+		operator bool() const
+		{
+			return s;
+		}
+		U operator*() const
+		{
+			return f(*s);
+		}
+		apply& operator++()
+		{
+			++s;
+
+			return *this;
+		}
+	};
+
+	// right fold using binop
+	template<class Op, class S, class T = S::value_type>
+	class fold {
+		Op op;
+		S s;
+		T t;
+	public:
+		using iterator_concept = std::forward_iterator_tag;
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = T;
+
+		fold()
+		{ }
+		fold(const Op& op, const S& _s, value_type t0)
+			: op(op), s(_s), t(t0)
+		{
+			if (s) {
+				t = op(t0, *s);
+			}
+		}
+		fold(const fold&) = default;
+		fold& operator=(const fold&) = default;
+		~fold()
+		{ }
+
+		operator bool() const
+		{
+			return s;
+		}
+		T operator*() const
+		{
+			return t;
+		}
+		fold& operator++()
+		{
+			if (++s) {
+				t = op(t, *s);
+			}
+
+			return *this;
+		}
+	};
+
+	/*
+	template<sequence S>
+	inline auto coro(S s)
+	{
+		while (s) {
+			yield* s;
+			++s;
+		}
+	}
+	*/
+
+#if 0
 
 	template<sequence S>
 	class counted {
@@ -32,7 +267,7 @@ namespace iterabool {
 		{ }
 		counted(const counted&) = default;
 		counted& operator=(const counted&) = default;
-		counted(counted&&) noexcept = default;
+		counted(counted&&)  = default;
 		counted& operator=(counted&&) = default;
 		~counted()
 		{ }
@@ -62,5 +297,5 @@ namespace iterabool {
 	};
 
 
-
+#endif
 }
--- iterabool.t.cpp
+++ iterabool.t.cpp
@@ -4,27 +4,132 @@
 
 using namespace iterabool;
 
-int test_constructor()
+int test_iota()
+{
+	{
+		iota<int> i;
+		assert(i);
+		iota i2(i);
+		assert(i2);
+		assert(i2 == i);
+		i = i2;
+		assert(i == i2);
+		assert(*i == 0);
+		++i;
+		assert(i);
+		assert(*i == 1);
+	}
+	return 0;
+}
+int test_iota_ = test_iota();
+
+int test_array()
 {
 	{
 		int i[] = { 1,2,3 };
-		counted c(i, 3);
-		counted c2{ c };
+		array c(i);
+		array c2{ c };
+		assert(c2 == c);
 		c = c2;
+		assert(c == c2);
+		assert(c.size() == 3);
 		assert(c);
 		assert(*c == 1);
 		++c;
+		assert(c.size() == 2);
 		assert(c);
 		assert(*c == 2);
 		++c;
+		assert(c.size() == 1);
 		assert(c);
 		assert(*c == 3);
 		++c;
+		assert(c.size() == 0);
+		assert(!c);
+		++c;
+		assert(c.size() == 0);
 		assert(!c);
 	}
 
 	return 0;
 }
+int test_array_ = test_array();
+
+int test_take()
+{
+	{
+		int i[] = { 1,2,3 };
+		array a(i);
+		size_t n = 2;
+		take c(n, a);
+		take c2(c);
+		assert(c2 == c);
+		c = c2;
+		assert(c == c2);
+
+		assert(c.size() == 2);
+		assert(c);
+		assert(*c == 1);
+		++c;
+		assert(c.size() == 1);
+		assert(c);
+		assert(*c == 2);
+		++c;
+		assert(c.size() == 0);
+		assert(!c);
+		++c;
+		assert(c.size() == 0);
+		assert(!c);
+	}
+
+	return 0;
+}
+int test_take_ = test_take();
+
+int test_apply()
+{
+	{
+		auto s = take(3, iota<int>{});
+		auto t = apply([](int i) { return i * i; }, s);
+		assert(t);
+		assert(*t == 0);
+		++t;
+		assert(t);
+		assert(*t == 1);
+		++t;
+		assert(*t == 4);
+		++t;
+		assert(!t);
+		++t;
+		assert(!t);
+	}
+
+	return 0;
+}
+
+int test_fold()
+{
+	{
+		auto f = fold(std::plus<int>{}, take(3, iota<int>(2)), 0);
+		auto f2(f);
+		f = f2;
+		assert(f);
+		assert(*f == 2);
+		++f;
+		assert(f);
+		assert(*f == 2 + 3);
+		++f;
+		assert(f);
+		assert(*f == 2 + 3 + 4);
+		++f;
+		assert(!f);
+		++f;
+		assert(!f);
+	}
+
+	return 0;
+}
+int test_fold_ = test_fold();
 
 int main()
 {
